5cc0d89c8a1533fc5e280852e4eb2f1fda1693ab
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2023-11-09 17:05:09 +0530

    updated

---
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -0,0 +1,11 @@
+#Load data 
+
+class DataIngestion:
+
+    def __init__(self):
+        pass
+
+    def initiate_data_ingestion(self):
+        pass
+
+    
---
+++ src/DimondPricePrediction/components/data_transformation.py
@@ -0,0 +1,9 @@
+
+
+class DataTransformation:
+    
+    def __init__(self):
+        pass
+
+    def get_data_transformation(self):
+        pass
