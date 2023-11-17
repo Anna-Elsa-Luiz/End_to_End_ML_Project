a474f864158501874884ae69cc0f8a6caf52f35a
Anna Elsa Luiz <annaelsaluiz@gmail.com>
2023-11-17 16:36:24 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData, PredictPipeline
+    
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
 
-from flask import Flask, request, render_template, jsonify
+from flask import Flask,request,render_template,jsonify
 
-app = Flask(__name__)  # created an object of Flask
 
-# creating a route
+app=Flask(__name__)
 
 
 @app.route('/')
 def home_page():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/predict', methods=['GET', 'POST'])
+@app.route("/predict",methods=["GET","POST"])
 def predict_datapoint():
-    if request.method == 'GET':
-        return render_template('form.html')
+    if request.method == "GET":
+        return render_template("form.html")
+    
     else:
-        data = CustomData(
-
+        data=CustomData(
+            
             carat=float(request.form.get('carat')),
-            depth=float(request.form.get('depth')),
-            table=float(request.form.get('table')),
-            x=float(request.form.get('x')),
-            y=float(request.form.get('y')),
-            z=float(request.form.get('z')),
-            cut=request.form.get('cut'),
-            color=request.form.get('color'),
-            clarity=request.form.get('clarity')
+            depth = float(request.form.get('depth')),
+            table = float(request.form.get('table')),
+            x = float(request.form.get('x')),
+            y = float(request.form.get('y')),
+            z = float(request.form.get('z')),
+            cut = request.form.get('cut'),
+            color= request.form.get('color'),
+            clarity = request.form.get('clarity')
         )
         # this is my final data
-        final_data = data.get_data_as_dataframe()
+        final_data=data.get_data_as_dataframe()
+        
+        predict_pipeline=PredictPipeline()
+        
+        pred=predict_pipeline.predict(final_data)
+        
+        result=round(pred[0],2)
+        
+        return render_template("result.html",final_result=result)
 
-        predict_pipeline = PredictPipeline()
-
-        pred = predict_pipeline.predict(final_data)
-
-        result = round(pred[0], 2)
-
-        return render_template("result.html", final_result=result)
-
-
-# execution begin
+#execution begin
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0",port=8080)
